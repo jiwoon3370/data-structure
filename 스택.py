0049cc48530db318fbee8df23754164f344f6970
nikmak45 <74951332+jiwoon3370@users.noreply.github.com>
2025-07-05 12:06:25 +0900

    Create 스택.py
    correction은 만들지 않았습니다.

---
+++ 스택.py
@@ -0,0 +1,116 @@
+# 짝 홀 구분될 리스트 선언
+A = []
+B = []
+
+# 스택 정수 원소들 리스트로 받기
+while True:
+    try:
+        stack = list(map(int, input("스택 정수 원소들 입력: ").split()))
+        print("입력된 스택:", stack)
+        break
+    
+    # 예외처리 (정수가 아님)
+    except ValueError:
+        print("잘못된 입력입니다. 정수만 입력해주세요.")
+        stack = []
+
+# 원소들의 짝 홀 구분으로 스택 A, B 나누기
+for i in range(len(stack)):
+    if stack[i] %2 == 0:
+        A.append(stack[i])
+    else :
+        B.append(stack[i])
+        
+print(A)
+print(B)
+
+# 스택 명령어 class 정의
+class steak_Manager :
+    def __init__(self, steak_A, steak_B):
+        self.steak_A = steak_A
+        self.steak_B = steak_B
+    
+    # 추가
+    def add():
+        element = []
+        element = list(map(int, input("추가할 원소 입력: ").split()))
+        print(element)
+        for i in range(len(element)):
+            if element[i] %2 == 0:
+                A.append(element[i])
+            else :
+                B.append(element[i])
+                
+        print(A)
+        print(B)
+        
+    # 삭제
+    def removes():
+        while True :
+            try:
+                prompt = list(input("제거할 스택과 수량 입력(예시 : A 4) : ").split())
+                length = len(prompt)
+                break
+            except length > 2:
+                print("잘못된 입력입니다ㅑ")
+                prompt = []
+            
+        print(prompt[0])
+        print(prompt[1])
+        
+        count = int(prompt[1])
+        if prompt[0] == "A" :
+            del A[-count:]
+            
+        elif prompt[0] == "B" :
+            del B[-count:]
+        print(A)
+        print(B)
+        
+    # 수정(미완)
+    def correction ():
+        while True:
+            try : 
+                cor_element = int(input("수정할 원소 입력:"))
+                print("입력한 원소 : " , cor_element)
+                break
+            except ValueError:
+                print("정수를 입력해주세요")
+                cor_element = None
+                
+        if cor_element in A:
+            
+            pass
+        
+    # 출력
+    def prints ():
+        print(A)
+        print(B)
+        
+    # 종료
+    def ends ():
+        print("프로젝트를 종료합니다")
+        exit()
+
+# 스택 명령어 대기
+manager = steak_Manager(A, B)
+
+# 스택 명령어 입력
+while True : 
+    print("명령을 입력하세요")
+    other = input(" adds  removes  correction  prints  ends  ")
+    if other == "adds" :
+        steak_Manager.add()
+        other = None
+    elif other == "removes" :
+        steak_Manager.removes()
+        other = None
+    elif other == "correction" :
+        steak_Manager.correction()
+        other = None
+    elif other == "ends" :
+        steak_Manager.ends()
+        other = None
+    elif other == "prints" :
+        steak_Manager.prints()
+        other = None
